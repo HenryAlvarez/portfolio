0eb2b53c79e80fe5941db465d1a0996359292acc
Henry <henry.gabriel.alvarez@gmail.com>
2025-04-02 22:35:36 -0600

    haslink contact

--- src/components/Services.js
+++ src/components/Services.js
@@ -52,7 +52,7 @@ export default function Services() {
             <h4 className="text-lg mt-2">{servicio.subtitulo}</h4>
             <p className="text-secondary mt-4">{servicio.descripcion}</p>
             <a
-              href="#contacto"
+              href="#contact"
               className={`inline-block mt-6 px-4 py-2 font-semibold rounded-lg ${
                 servicio.destacado
                   ? "bg-background text-highlight"
